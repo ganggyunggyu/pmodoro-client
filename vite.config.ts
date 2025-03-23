b04e28a0e6189be5a1a1682a3bd18baf7f5cb4b7
ganggyunggyu <qwzx16@naver.com>
2025-03-23 18:03:30 +0900

    chore: init pmodoro-react project

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+// import { defineConfig } from 'vite';
+// import tailwindcss from '@tailwindcss/vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: [
+//       { find: '@', replacement: '/src' },
+//       { find: '@views', replacement: '/src/views' },
+//       { find: '@resource', replacement: '/src/static/resource' },
+//     ],
+//   },
+// });
+
+import fs from 'fs';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+const cert = fs.readFileSync('localhost.pem');
+const key = fs.readFileSync('localhost-key.pem');
+
+export default {
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@views', replacement: '/src/views' },
+      { find: '@resource', replacement: '/src/static/resource' },
+    ],
+  },
+  server: {
+    host: true,
+    https: {
+      key,
+      cert,
+    },
+  },
+  host: '0.0.0.0',
+  port: 5173,
+};

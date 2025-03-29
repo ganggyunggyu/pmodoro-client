42be355e115d434baa103eb7d26c2b305f791951
ganggyunggyu <qwzx16@naver.com>
2025-03-29 15:45:02 +0900

    style: header/mainprovider 분리

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,23 @@ import { KakaoCallbackPage } from './pages/kakaoCallbackPage';
 import { ProfilePage } from './pages/ProfilePage';
 import { HomePage } from './pages/HomePage';
 import { ChatPage } from './pages/ChatPage';
+import React from 'react';
+import { Header } from './widgets/header';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/profile/:userId" element={<ProfilePage />} />
-      <Route path="/auth/kakao-callback" element={<KakaoCallbackPage />} />
-      <Route path="/chat/:userId/:roomId?" element={<ChatPage />} />
+    <main className="w-screen h-screen overflow-y-scroll">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+        <Route path="/auth/kakao-callback" element={<KakaoCallbackPage />} />
+        <Route path="/chat/:userId/:roomId?" element={<ChatPage />} />
 
-      <Route path="/onboarding/*" element={<OnboardingWidget />} />
-    </Routes>
+        <Route path="/onboarding/*" element={<OnboardingWidget />} />
+      </Routes>
+    </main>
   );
 }
 

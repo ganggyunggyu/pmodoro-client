570c994c65631a369bff9da16c676f4468127b58
ganggyunggyu <qwzx16@naver.com>
2025-03-29 15:49:20 +0900

    chore: userinfo type update

--- src/app/store/useUserStore.ts
+++ src/app/store/useUserStore.ts
@@ -1,8 +1,8 @@
 import { create } from 'zustand';
 
 export type UserInfo = {
-  _id: string; //mongoid
-  userId: string; //kakaoid
+  _id: string;
+  userId: string;
   displayName: string;
   phoneNumber?: string;
   firstArea?: string;
@@ -10,6 +10,22 @@ export type UserInfo = {
   position: string;
   detailPositionList: string[];
   career: number;
+  job: string;
+  Introduce: string;
+
+  //로컬 로그인 스테이트
+  email: string;
+  password: string;
+
+  // 카카오 관련 선택 필드
+  kakaoId?: number;
+  profileImg?: string;
+  auth_time?: number;
+  exp?: number;
+  iat?: number;
+  iss?: string;
+  sub?: string;
+  aud?: string;
 };
 
 export type UserState = {

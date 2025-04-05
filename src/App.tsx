563a8b2a534caf5641b2b6fd4acc128117aaa271
ganggyunggyu <qwzx16@naver.com>
2025-04-05 21:23:11 +0900

    feat: login page -> widget

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import './App.css';
 import { OnboardingWidget } from './widgets/onboarding';
 import { LoginPage } from './pages/LoginPage';
 import { KakaoCallbackPage } from './pages/kakaoCallbackPage';
-import { ProfilePage } from './pages/ProfilePage';
 import { HomePage } from './pages/HomePage';
 import { ChatPage } from './pages/ChatPage';
 import { Header } from './widgets/header';
+import { useWidgetStore } from './app/store';
+import { Mypage } from './pages/Mypage';
+import { ProfilePage } from './pages/ProfilePage';
 
 export const Footer = () => {
   return (
-    <footer className="absolute bottom-0 w-screen h-20 px-10 flex justify-between bg-primary-mute text-white text-xs">
+    <footer className="absolute bottom-0 w-screen h-20 px-[10%] flex justify-between bg-primary-mute text-white text-xs">
       <article className="flex items-center gap-3">
         <button>개인정보 처리방침</button>
         <div className="h-3 w-[1px] bg-white" />
@@ -27,13 +29,15 @@ export const Footer = () => {
 };
 
 function App() {
+  const { isLoginWidgetOpen } = useWidgetStore();
   return (
     <React.Fragment>
       <Header />
-      <main className="fixed w-screen h-[calc(100vh-var(--spacing)*16)] overflow-y-scroll pt-16">
+      {isLoginWidgetOpen && <LoginPage />}
+      <main className="fixed w-screen h-[calc(100vh-var(--spacing)*16)] overflow-y-scroll pt-24 px-[10%]">
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/my-page/:userId" element={<Mypage />} />
           <Route path="/profile/:userId" element={<ProfilePage />} />
           <Route path="/auth/kakao-callback" element={<KakaoCallbackPage />} />
           <Route path="/chat/:userId/:roomId?" element={<ChatPage />} />

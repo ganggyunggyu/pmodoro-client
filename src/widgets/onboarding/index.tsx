ecaa66546de91cbb360c0f9f242864e5580d7af5
ganggyunggyu <qwzx16@naver.com>
2025-04-05 22:35:10 +0900

    fix: build

--- src/widgets/onboarding/index.tsx
+++ src/widgets/onboarding/index.tsx
@@ -3,10 +3,8 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 
 import { Step1UserInfo } from './step-1-user-info';
 import { Step2Position } from './step-2-position';
-import { Step3PositionDetail } from './step-3-position-detail';
 import { Step4Project } from './step-4-project';
 import { Step5Final } from './step-5-final';
-import { Step3_1PositionDeveloper } from './step-3-1-developer';
 import { Step0Auth } from './step-0-auth';
 
 import { StatusBar, ActionButtonList } from './ui';
@@ -29,11 +27,6 @@ export const OnboardingWidget = () => {
         <Route path="auth" element={<Step0Auth />} />
         <Route path="user-info" element={<Step1UserInfo />} />
         <Route path="position" element={<Step2Position />} />
-        <Route path="position/:position" element={<Step3PositionDetail />} />
-        <Route
-          path="position/developer/tech-stack"
-          element={<Step3_1PositionDeveloper />}
-        />
         <Route path="other-info" element={<Step4Project />} />
         <Route path="final" element={<Step5Final />} />
       </Routes>

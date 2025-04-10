a4f996a9d0df45f76c4ce63428341fbbf7f25c9c
ganggyunggyu <qwzx16@naver.com>
2025-04-10 18:07:43 +0900

    chore: update .env

--- src/shared/components/KakaoLoginButton.tsx
+++ src/shared/components/KakaoLoginButton.tsx
@@ -1,9 +1,7 @@
-import { KAKAO_REDIRECT_URI } from '../constants/env-config';
-
 const KakaoLoginButton = () => {
-  const CALLBACK = `https://kauth.kakao.com/oauth/authorize?client_id=592b7c49df0845263bf62a37723069f2&redirect_uri=${
-    import.meta.env.VITE_KAKAO_REDIRECT_URI
-  }&response_type=code`;
+  const CALLBACK = `https://kauth.kakao.com/oauth/authorize?client_id=${
+    import.meta.env.VITE_KAKAO_CLIENT_ID
+  }&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URI}&response_type=code`;
   const handleKakaoLogin = () => {
     window.location.href = CALLBACK;
   };

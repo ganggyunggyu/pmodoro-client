f5415f8b255137fcfa424d80b1d31e0b9b638414
ganggyunggyu <qwzx16@naver.com>
2025-04-11 15:47:17 +0900

    refactor: app 모듈 내보내기

---
+++ src/app/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useLocation } from 'react-router';
+
+import { Header } from '../widgets/header';
+import { useWidgetStore } from './store';
+
+import { useChatStore } from './store/useChatStore';
+import { getIsMobile } from '../shared/lib';
+import { Footer } from '../widgets/footer';
+import { Routing } from '../pages';
+import { LoginPage } from '../pages/login-page';
+import { ChatHeader } from '../widgets/chat-header';
+import { RouteProvider } from './provider/router-provider';
+import { AuthProvider } from './provider/auth-provider';
+
+export const App = () => {
+  const location = useLocation();
+  const { pathname } = location;
+  const { isLoginWidgetOpen, setIsLoginWidgetOpen } = useWidgetStore();
+  const { currentRoomId } = useChatStore();
+
+  React.useEffect(() => {
+    if (isLoginWidgetOpen) setIsLoginWidgetOpen(false);
+  }, [pathname]);
+
+  const isChat = location.pathname.includes('chat');
+
+  const isMobile = getIsMobile();
+
+  return (
+    <AuthProvider>
+      {isMobile && isChat && currentRoomId ? <ChatHeader /> : <Header />}
+      {isLoginWidgetOpen && <LoginPage />}
+      <RouteProvider>
+        <Routing />
+      </RouteProvider>
+      {!isChat && <Footer />}
+    </AuthProvider>
+  );
+};

a81b59472e2fe6ee08e6afb0828d07f30f5954f4
ganggyunggyu <qwzx16@naver.com>
2025-04-11 15:05:32 +0900

    refactor: user api-hooks add

--- src/entities/user/hooks/user.hooks.ts
+++ src/entities/user/hooks/user.hooks.ts
@@ -1,1 +1,9 @@
-export const hooksuserFn = () => {}
+import { useQuery } from '@tanstack/react-query';
+import { getUser } from '../api';
+
+export const useGetUserQuery = (userId: string) => {
+  return useQuery({
+    queryKey: [userId],
+    queryFn: () => getUser(userId),
+  });
+};

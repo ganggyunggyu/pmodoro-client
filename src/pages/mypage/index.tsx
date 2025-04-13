ed519f5d118a2dfcfe904f195b796e9d54c5c2dd
ganggyunggyu <qwzx16@naver.com>
2025-04-13 11:07:23 +0900

    refactor: edit input tree

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,80 +1,26 @@
 import React, { Fragment } from 'react';
-import { useNavigate, useParams, useLocation } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
-import { UserInfo, useUserStore } from '@/app/store/useUserStore';
-import { useQuery } from '@tanstack/react-query';
+import { useUserStore } from '@/app/store/useUserStore';
+
 import { PulseLoaderSpinner } from '@/shared/components/PulseLoaderPage';
 import { getIsMobile } from '@/shared/lib';
 
 import { ProjectForm } from '@/features/project/ui/project-form';
 import { useWidgetStore } from '@/app/store';
 import {
-  Project,
-  useDeleteProject,
   useGetProjectByUser,
   useGetUserQuery,
   usePatchUserMutation,
 } from '@/entities';
 import { ProfileImage } from '@/entities/user/ui/profile-image';
 import { ProjectCard } from '@/features/project/ui/project-card';
-import { NextIcon, XIcon } from '@/widgets/onboarding/step-1-user-info';
-import { DEVELOPER_POSITIONS } from '@/shared/constants/positions';
+import { NextIcon } from '@/widgets/onboarding/step-1-user-info';
+
 import { PositionSelector } from '@/widgets/selector/position';
 import { MainSidebar } from '@/widgets/side-bar/main-side-bar';
+import { EditInput } from '@/shared/components/EditInput';
 
-interface EditInputProps {
-  label: string;
-  value: string;
-  isEditing: boolean;
-  onChange: (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => void;
-}
-
-export const EditInput: React.FC<EditInputProps> = ({
-  label,
-  value,
-  isEditing,
-  onChange,
-}) => {
-  const textareaRef = React.useRef<HTMLTextAreaElement>(null);
-  React.useEffect(() => {
-    const textarea = textareaRef.current;
-    if (textarea) {
-      textarea.style.height = 'auto'; // 일단 높이 초기화
-      textarea.style.height = `${textarea.scrollHeight}px`; // 스크롤 높이만큼 다시 설정
-    }
-  }, [value]);
-  return (
-    <div className="flex w-full justify-start lg:gap-30 items-center">
-      <p className="w-40 text-black-alt">{label}</p>
-      <div
-        className={`w-full p-2 ${
-          isEditing ? 'border border-alt rounded-md' : ''
-        }`}
-      >
-        {label === '자기소개' ? (
-          <textarea
-            ref={textareaRef}
-            value={value}
-            onChange={onChange}
-            placeholder="자기소개"
-            className="w-full rounded-lg"
-            disabled={!isEditing}
-          />
-        ) : (
-          <input
-            type="text"
-            value={value}
-            className="w-full"
-            disabled={!isEditing}
-            onChange={onChange}
-          />
-        )}
-      </div>
-    </div>
-  );
-};
 export const PositionLabelList = () => {
   const { userInfo } = useUserStore();
   const userId = userInfo._id;

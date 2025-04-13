7ff3f22ba4860863f47837deea109e12418ca597
ganggyunggyu <qwzx16@naver.com>
2025-04-13 11:30:21 +0900

    refactor: position ui tree

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { useUserStore } from '@/app/store/useUserStore';
@@ -15,62 +15,11 @@ import {
 } from '@/entities';
 import { ProfileImage } from '@/entities/user/ui/profile-image';
 import { ProjectCard } from '@/features/project/ui/project-card';
-import { NextIcon } from '@/widgets/onboarding/step-1-user-info';
 
 import { PositionSelector } from '@/widgets/selector/position';
 import { MainSidebar } from '@/widgets/side-bar/main-side-bar';
 import { EditInput } from '@/shared/components/EditInput';
-
-export const PositionLabelList = () => {
-  const { userInfo } = useUserStore();
-  const userId = userInfo._id;
-  const userQuery = useGetUserQuery(userId);
-
-  if (userQuery.isLoading) return <p>loading</p>;
-
-  return (
-    <React.Fragment>
-      <div className="flex w-full justify-start lg:gap-25 items-center">
-        <p className="w-30 text-black-alt">상세 포지션</p>
-        <div className="flex gap-3">
-          {userInfo.skills?.map((position, index) => {
-            return (
-              <button
-                key={index}
-                className="text-xs py-2 px-3 rounded-full bg-white border border-alt"
-              >
-                {position}
-              </button>
-            );
-          })}
-        </div>
-      </div>
-    </React.Fragment>
-  );
-};
-
-export const PositionSelectorButton = () => {
-  const { userInfo } = useUserStore();
-
-  const { setIsPositionSelector } = useWidgetStore();
-
-  return (
-    <article className="flex w-full justify-start lg:gap-30 items-center">
-      <p className="w-40 text-black-alt">상세 포지션</p>
-      <button
-        onClick={() => {
-          setIsPositionSelector(true);
-        }}
-        className="flex justify-between p-3 w-full text-left border border-alt rounded-lg"
-      >
-        <p className={`${userInfo.skills ? 'text-black-alt' : 'text-black'}`}>
-          {userInfo.skills ? userInfo.skills?.join(', ') : '기술 스택'}
-        </p>
-        <NextIcon />
-      </button>
-    </article>
-  );
-};
+import { PositionLabelList } from '@/features/position/ui/position-label-list';
 
 export const Mypage: React.FC = () => {
   const isMobile = getIsMobile();
@@ -227,7 +176,7 @@ export const Mypage: React.FC = () => {
                 disabled={!isEditing}
               />
             </div>
-            {!isEditing ? <PositionLabelList /> : <PositionSelectorButton />}
+            {!isEditing ? <PositionLabelList /> : <PositionSelector />}
           </article>
         </div>
         <p className="w-full text-left text-xl font-bold py-5">프로젝트 이력</p>

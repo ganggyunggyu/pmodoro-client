c696bf4e3dc204e52e7c732089c6bce308343d0f
ganggyunggyu <qwzx16@naver.com>
2025-04-20 14:46:14 +0900

    refactor: 공통 컴포넌트 사용 및 스타일 수정

--- src/features/position/ui/position-selector-button/index.tsx
+++ src/features/position/ui/position-selector-button/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useWidgetStore } from '@/app/store';
 import { useUserStore } from '@/app/store/useUserStore';
-import { NextIcon } from '@/shared';
+import { LabelButton, NextIcon, SelectorButton } from '@/shared';
+import { PositionLabelList } from '../position-label-list';
 
 export const PositionSelectorButton = () => {
   const { userInfo } = useUserStore();
@@ -9,21 +10,25 @@ export const PositionSelectorButton = () => {
   const { setIsPositionSelector } = useWidgetStore();
 
   return (
-    <React.Fragment>
-      <article className="flex w-full justify-start lg:gap-30 items-center">
-        <p className="w-40 text-black-alt">상세 포지션</p>
-        <button
-          onClick={() => {
-            setIsPositionSelector(true);
-          }}
-          className="flex justify-between p-3 w-full text-left border border-alt rounded-lg"
-        >
-          <p className={`${userInfo.skills ? 'text-black-alt' : 'text-black'}`}>
-            {userInfo.skills ? userInfo.skills?.join(', ') : '기술 스택'}
-          </p>
-          <NextIcon />
-        </button>
-      </article>
-    </React.Fragment>
+    <article className="flex w-full justify-start lg:gap-30 items-center">
+      <p className="w-40 text-black-alt">상세 포지션</p>
+
+      <SelectorButton
+        onClick={() => {
+          setIsPositionSelector(true);
+        }}
+        icon="arrow"
+        isSelected={false}
+      >
+        <p className={`${userInfo.skills ? 'text-black-alt' : 'text-black'}`}>
+          {userInfo.skills ? userInfo.skills?.join(', ') : '기술 스택'}
+        </p>
+      </SelectorButton>
+      {/* <div className="absolute w-full bottom-0">
+        {userInfo?.skills.map((skil) => {
+          return <LabelButton variant="outlineAlt" size="sm" label={skil} />;
+        })}
+      </div> */}
+    </article>
   );
 };
--- src/widgets/selector/position/index.tsx
+++ src/widgets/selector/position/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 import { useWidgetStore } from '@/app/store';
 import { useUserStore } from '@/app/store/useUserStore';
 import { useGetUserQuery } from '@/entities';
@@ -8,12 +8,11 @@ import { Button, SelectorButton, XIcon } from '@/shared';
 export const PositionSelector = () => {
   const { userInfo, setUserInfo } = useUserStore();
   const { setIsPositionSelector } = useWidgetStore();
-  // const userId = userInfo?._id;
-  // const { isLoading } = useGetUserQuery(userId);
 
   const handlePositionSelect = () => setIsPositionSelector(false);
 
-  const toggleSkill = (position: string) => {
+  const toggleSkill = (e: React.MouseEvent, position: string) => {
+    e.stopPropagation();
     const { skills } = userInfo;
     const isSelect = skills.includes(position);
 
@@ -25,20 +24,20 @@ export const PositionSelector = () => {
   };
 
   return (
-    // <article className="fixed w-full lg:w-1/2 md:w-2/3 top-1/2 left-1/2 -translate-1/2 bg-white transition-all rounded-md p-3">
-    <article className="shadow-md flex flex-col gap-5 p-4">
+    <article className="fixed flex flex-col gap-5 w-full lg:w-1/2 md:w-2/3 top-1/2 left-1/2 -translate-1/2 bg-white transition-all rounded-md p-3">
+      {/* <article className="shadow-md flex flex-col gap-5 p-4"> */}
       <header className="w-full flex justify-between">
         <p className="text-black text-lg font-semibold">기술 스택</p>
         <button onClick={handlePositionSelect}>
           <XIcon />
         </button>
       </header>
-      <ul className="h-60 flex flex-col gap-2 overflow-y-scroll">
+      <ul className="h-60 flex flex-col gap-2 overflow-y-scroll p-2">
         {DEVELOPER_POSITIONS.map((position) => (
           <SelectorButton
             key={position}
             isSelected={userInfo?.skills?.includes(position)}
-            onClick={() => toggleSkill(position)}
+            onClick={(e) => toggleSkill(e, position)}
             icon="check"
           >
             <p>{position}</p>
@@ -67,15 +66,16 @@ export const PositionSelectorWrapper = () => {
 
   if (isLoading) return <p>loading</p>;
 
-  const handlePositionSelect = () => setIsPositionSelector(false);
+  const handlePositionSelect = () => {
+    setIsPositionSelector(false);
+  };
 
   return (
-    <>
-      <div
-        onClick={handlePositionSelect}
-        className="fixed top-0 left-0 w-screen h-screen bg-black opacity-30 z-10 "
-      />
+    <div
+      onClick={handlePositionSelect}
+      className="fixed top-0 left-0 w-screen h-screen bg-black/30 z-10 "
+    >
       <PositionSelector />
-    </>
+    </div>
   );
 };

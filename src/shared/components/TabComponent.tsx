8dbc3cccde17e98cbe7ec1633a180e3a448822a0
ganggyunggyu <qwzx16@naver.com>
2025-04-23 17:21:37 +0900

    refactor: 탭아이템 범용성을 위해 props 추가

--- src/shared/components/TabComponent.tsx
+++ src/shared/components/TabComponent.tsx
@@ -13,53 +13,34 @@ export const TabComponent = ({ tabOptionList }) => {
   return (
     <div className="flex">
       {tabOptionList.map((tab) => (
-        <TabItem tab={tab} />
+        <TabItem isActive={false} tab={tab} />
       ))}
     </div>
   );
 };
-
-interface TabItemProps {
-  tab: any;
+interface TabItemProps extends React.HTMLAttributes<HTMLDivElement> {
+  tab: {
+    id: string | number;
+    label: string;
+  };
+  isActive: boolean;
 }
 
-export const useUserSearchQuery = () => {
-  const { searchQuery } = useSearchStore(); // zustand store에서 searchQuery 가져오기
-
-  return useQuery({
-    queryKey: ['searchUsers', searchQuery], // 쿼리 키로 searchQuery를 사용
-    queryFn: () => getSearchUsers(searchQuery), // 서버 요청
-  });
-};
-
-export const getSearchUsers = async (searchQuery: Record<string, any>) => {
-  const { data } = await axios.get('/users/search', { params: searchQuery });
-  return data;
-};
-
-export const TabItem: React.FC<TabItemProps> = ({ tab }) => {
-  const { setSearchQueryField, searchQuery } = useSearchStore();
-
-  const handleTabClick = (position) => {
-    setSearchQueryField('position', position);
-  };
-
-  const getIsActive = () => {
-    return searchQuery.position === tab.label;
-  };
-  const isActiveTemp = getIsActive();
-
+export const TabItem: React.FC<TabItemProps> = ({
+  tab,
+  className,
+  isActive,
+  ...props
+}) => {
   return (
     <div
       key={tab.id}
-      className={`cursor-pointer p-2 mx-2 text-xs lg:text-sm ${
-        isActiveTemp
-          ? 'text-red-500 border-b-2 border-red-500'
-          : 'text-gray-500'
-      }`}
-      onClick={() => handleTabClick(tab.label)}
+      className={`cursor-pointer p-2 lg:mx-2 text-caption1-m lg:text-headline-m ${
+        isActive ? 'text-red-500 border-b-2 border-red-500' : 'text-gray-500'
+      } ${className ?? ''}`}
+      {...props}
     >
-      <p className="">{tab.label}</p>
+      <p className="w-fit">{tab.label}</p>
     </div>
   );
 };
--- src/widgets/user-search-widget/index.tsx
+++ src/widgets/user-search-widget/index.tsx
@@ -2,23 +2,50 @@ import React from 'react';
 import { UserSearchForm } from '@/features/user/ui';
 import { TabComponent, TabItem } from '@/shared/components/TabComponent';
 import { useSearchStore } from '@/app/store/useSearchStore';
+import { useQuery } from '@tanstack/react-query';
+import { axios } from '@/app/config';
 
+export const tabs = [
+  { id: 'all', label: '전체' },
+  { id: 'developer', label: '개발자' },
+  { id: 'planner', label: '기획자' },
+  { id: 'designer', label: '디자이너' },
+  { id: 'marketer', label: '마케터' },
+];
+export const useUserSearchQuery = () => {
+  const { searchQuery } = useSearchStore(); // zustand store에서 searchQuery 가져오기
+
+  return useQuery({
+    queryKey: ['searchUsers', searchQuery], // 쿼리 키로 searchQuery를 사용
+    queryFn: () => getSearchUsers(searchQuery), // 서버 요청
+  });
+};
+
+export const getSearchUsers = async (searchQuery: Record<string, any>) => {
+  const { data } = await axios.get('/users/search', { params: searchQuery });
+  return data;
+};
 export const UserSearchWidget: React.FC = () => {
-  const tabs = [
-    { id: 'all', label: '전체' },
-    { id: 'developer', label: '개발자' },
-    { id: 'planner', label: '기획자' },
-    { id: 'designer', label: '디자이너' },
-    { id: 'marketer', label: '마케터' },
-  ];
+  const { setSearchQueryField, searchQuery } = useSearchStore();
+  const handleTabClick = (position: string) => {
+    setSearchQueryField('position', position);
+  };
+  const getIsActive = (position) => {
+    return searchQuery.position === position;
+  };
 
   return (
-    <section className="flex flex-col gap-5 mb-5 w-full overflow-scroll">
+    <section className="flex flex-col gap-5 mb-5 min-w-full overflow-scroll">
       <p className="text-xl font-semibold">원하는 팀원을 구체적으로 검색해요</p>
 
-      <div className="flex">
+      <div className="flex items-center justify-between lg:justify-start w-full">
         {tabs.map((tab) => (
-          <TabItem tab={tab} key={tab.id} />
+          <TabItem
+            isActive={getIsActive(tab.label)}
+            onClick={() => handleTabClick(tab.label)}
+            tab={tab}
+            key={tab.id}
+          />
         ))}
       </div>
       <UserSearchForm />

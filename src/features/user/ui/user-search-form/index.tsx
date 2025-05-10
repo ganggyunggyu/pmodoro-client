31d12bf0f4104f4d68e8c5e1109acf24dab56110
ganggyunggyu <qwzx16@naver.com>
2025-05-10 15:00:45 +0900

    feat: 기술 스택 검색기능 추가

--- src/features/user/ui/user-search-form/index.tsx
+++ src/features/user/ui/user-search-form/index.tsx
@@ -1,45 +1,151 @@
-import React from 'react';
-import { DropDownButton, DropDownOverlay } from '@/shared/components/DropDown';
 import { useWidgetStore } from '@/app/store';
+import { Button, DropdownButton, SelectorButton, XIcon } from '@/shared';
+import { useSearchStore } from '@/app/store/useSearchStore';
+import { DropDownButton } from '@/shared/components/DropDown';
+import { DropdownWrapper } from '@/pages/components-page';
+import { TabItem } from '@/shared/components/TabComponent';
+import { tabs } from '@/widgets/user-search-widget';
+import React from 'react';
+import {
+  ALL_POSITIONS,
+  DESIGNER_POSITIONS,
+  DEVELOPER_POSITIONS,
+  MARKETER_POSITIONS,
+  PLANNER_POSITIONS,
+} from '@/shared/constants/positions';
+import { useUserStore } from '@/app/store/useUserStore';
 
 export const UserSearchForm = () => {
-  // Zustand 스토어에서 상태 가져오기
   const { isSearchDropDownOpen, setIsSearchDropDownOpen } = useWidgetStore();
 
-  // 옵션 및 포지션 리스트
-  const options = [
-    // { id: 'all', label: '기술 스택' },
-    // { id: 'developer', label: '세부 직무' },
-    // { id: 'planner', label: '경력' },
-    { id: 'designer', label: '온라인' },
-    { id: 'marketer', label: '오프라인' },
-    // { id: 'marketer', label: '위치' },
+  const { setSearchQueryField, searchQuery, toggleSkill } = useSearchStore();
+  const [selectedPosition, setSelectedPosition] = React.useState('전체');
+
+  const [isPositionDropdownOpen, setIsPositionDropdownOpen] =
+    React.useState(false);
+  const [isCareerDropdownOpen, setIsCareerDropdownOpen] = React.useState(false);
+
+  const [isUserSkillDropdownOpen, setIsUserSkillDropdownOpen] =
+    React.useState(false);
+
+  const onOffButtonList = [
+    { id: 'designer', label: '온라인', isOnline: true },
+    { id: 'marketer', label: '오프라인', isOnline: false },
   ];
 
-  const PositionList = [
-    { id: 1, label: '개발자' },
-    { id: 2, label: '디자이너' },
-    { id: 3, label: '기획자' },
-    { id: 4, label: '마케터' },
-  ];
+  const toggleOnlineOffline = (label: string) => {
+    if (searchQuery.isOnline === true && label === '온라인') {
+      setSearchQueryField('isOnline', null);
+    } else if (searchQuery.isOnline === false && label === '오프라인') {
+      setSearchQueryField('isOnline', null);
+    } else if (label === '온라인') {
+      setSearchQueryField('isOnline', true);
+    } else if (label === '오프라인') {
+      setSearchQueryField('isOnline', false);
+    }
+  };
 
-  const handleDropdownBackgroundClick = () => {
-    setIsSearchDropDownOpen(false);
+  const handleTabClick = (position) => {
+    setSelectedPosition(position);
+  };
+
+  const getSkillList = () => {
+    if (selectedPosition === '개발자') return DEVELOPER_POSITIONS;
+    if (selectedPosition === '디자이너') return DESIGNER_POSITIONS;
+    if (selectedPosition === '기획자') return PLANNER_POSITIONS;
+    if (selectedPosition === '마케터') return MARKETER_POSITIONS;
+    return ALL_POSITIONS;
+  };
+
+  const skillList = getSkillList();
+
+  const handleSkillClick = (skill) => {
+    toggleSkill(skill);
+
+    console.log(searchQuery);
   };
 
   return (
     <article className="flex gap-5">
-      {isSearchDropDownOpen && (
-        <DropDownOverlay
-          label="포지션"
-          tabOptionList={PositionList}
-          mainOptionList={options}
-          toggleDropdown={handleDropdownBackgroundClick}
-        />
-      )}
+      <div>
+        <DropdownWrapper
+          isOpen={isUserSkillDropdownOpen}
+          trigger={
+            <DropdownButton
+              onClick={() => {
+                setIsUserSkillDropdownOpen(!isUserSkillDropdownOpen);
+              }}
+              label="기술 스택"
+              direction={isUserSkillDropdownOpen ? 'up' : 'down'}
+              variant={isUserSkillDropdownOpen ? 'primaryTrans' : 'outlineAlt'}
+              size="sm"
+            />
+          }
+        >
+          <section className="flex flex-col gap-3 p-3 bg-white fixed">
+            <header className="w-full flex justify-between sticky top-0 bg-white z-10">
+              <p className="text-black text-lg font-semibold">포지션 선택</p>
+              <button
+                className="cursor-pointer"
+                onClick={() => setIsUserSkillDropdownOpen(false)}
+              >
+                <XIcon />
+              </button>
+            </header>
+            <div className="flex items-center justify-between w-full">
+              {tabs.map((tab) => {
+                return (
+                  <TabItem
+                    isActive={tab.label === selectedPosition}
+                    onClick={() => {
+                      handleTabClick(tab.label);
+                    }}
+                    tab={tab}
+                    key={tab.id}
+                  />
+                );
+              })}
+            </div>
+            <div className="flex flex-col gap-2 overflow-y-auto px-1 max-h-[300px]">
+              {skillList.map((skill, idx) => {
+                return (
+                  <SelectorButton
+                    key={idx}
+                    icon="check"
+                    isSelected={searchQuery.skills.includes(skill)}
+                    onClick={() => {
+                      handleSkillClick(skill);
+                    }}
+                  >
+                    {skill}
+                  </SelectorButton>
+                );
+              })}
+            </div>
 
-      {options.map((op, index) => (
-        <DropDownButton key={index} label={op.label} />
+            <div className="px-3 pb-3">
+              <Button
+                onClick={() => setIsUserSkillDropdownOpen(false)}
+                className="w-full py-3 bg-primary text-white rounded-md text-sm"
+              >
+                선택완료
+              </Button>
+            </div>
+          </section>
+        </DropdownWrapper>
+      </div>
+
+      <div className="w-0.5 bg-alt"></div>
+      {onOffButtonList.map((op, index) => (
+        <Button
+          key={index}
+          onClick={() => toggleOnlineOffline(op.label)}
+          size="sm"
+          children={op.label}
+          variant={
+            op.isOnline === searchQuery.isOnline ? 'primaryTrans' : 'outlineAlt'
+          }
+        />
       ))}
     </article>
   );
